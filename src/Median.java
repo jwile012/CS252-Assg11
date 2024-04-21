83f5e13346d2b84cbc8f63fb8607794f36beab1f
jwile012 <jwile012@odu.edu>
2024-04-21 03:48:13 -0400

    Assignment 12 Edits.

---
+++ src/Median.java
@@ -0,0 +1,38 @@
+package cs252;
+
+import java.util.Arrays;
+
+/**
+ * Class to compute median averages.
+ * 
+ * The median average of a sequence of numbers is the "middle" value
+ * if you have an odd number of values or the midpoint between the two middle
+ * values if you have an odd number of values.
+ */
+public class Median {
+
+    double[] values;
+
+    public Median(double[] values) {
+        this.values = values;
+    }
+
+    /**
+     * Compute the median average of the supplied values.
+     * @return the median average
+     */
+    double median() {
+        double[] sorted = new double[values.length];
+        for (int i = 0; i < values.length; ++i) {
+            sorted[i] = values[i];
+        }
+        Arrays.sort(sorted);
+        int middle = values.length / 2;
+        if (values.length % 2 == 0) {
+            // Adjusted index to middle - 1 for even-length arrays
+            return 0.5 * (sorted[middle - 1] + sorted[middle]);
+        } else {
+            return sorted[middle];
+        }
+    }
+}
